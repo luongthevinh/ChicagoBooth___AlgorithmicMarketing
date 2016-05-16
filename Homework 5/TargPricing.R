2e6ca951940a767c04bc33ae82235fa423d81578
Julio Andrés Guzmán Guzmán <julioa.guzmang@gmail.com>
2016-05-16 18:46:49 -0500

    Solution to Homework 4

--- Homework 5/TargPricing.R
+++ Homework 5/TargPricing.R
@@ -106,16 +106,229 @@ revhat[which.max(revhat)]
 revhat[which.max(revhat)]/pseq[which.max(revhat)]
 
 # Optimize
-optimize(f = predict.rev, interval = c(0,1000), jstate="TX", jcat=jcats[1], maximum = TRUE)
-
+optimize(f = predict.rev, interval = c(0,1000), jstate="NY", jcat=jcats[1], maximum = TRUE)
+# The revenue per customer is 28.76408
 
 # HOMEWORK
 # Use the Ziprecruiter case data and code to answer the following questions:
-#   
-# Q1: If the costs of servicing each customer are $10 (rather than zero) what is the optimal uniform price that Ziprecruiter should charge?
-# 
-# Q2: Ziprecruiter is deciding between segmenting their customer base either by state or by job category and then charging a flat fee for each group within the segment (i.e. for each state or category). In other words they would charge different uniform prices for each state or job category. Use the data and the code to justify which approach you think would get them higher expected revenues.
-# 
-# Q3: Does your answer to Q2 change if you assume marginal costs per customer are $10 and the decision was made based on profits rather than revenues?
+
+# Q1: If the costs of servicing each customer are $10 (rather than zero) 
+# what is the optimal uniform price that Ziprecruiter should charge?
+
+# Suggests inflexion around $249-$299 
+g2 = glm(SUB~prc,data=dz1,family='binomial')
+summary(g2)
+hist(predict(g2,type='response'))
+summary(predict(g2))
+
+# Construct Prediction Function for Profits with uniform pricing
+predict.profit = function(price, cost)
+{
+  #Supress Warnings
+  options(warn=-1)
+  # "New Data"
+  nd = data.frame(prc=price)
+  # Predict with the function only for price
+  phat = predict(g2,nd,type='response')  
+  # Reset Warnings
+  options(warn=0)
+  # Expected Profits
+  eprofit = as.numeric(phat*(price-cost))
+  return(eprofit)
+}
+##### Evaluating the function #####
+# price Seq
+pseq = seq(from=49,to=399,by=10)
+
+cost=0
+# Profits at each price
+revhat = predict.profit(pseq,cost)
+plot(revhat~pseq)
+# Optimal Price / Erev
+pseq[which.max(revhat)]
+revhat[which.max(revhat)]
+
+# probability of purchase
+revhat[which.max(revhat)]/pseq[which.max(revhat)]
+
+# Optimize
+optimize(f = predict.profit, interval = c(0,1000),cost, maximum = TRUE)
+
+# If cost=0, rev/cust = 40.09, price 294.0346
+# If cost=10, rev/cust = 38.74, price 302.691
 
 
+# Q2: Ziprecruiter is deciding between segmenting their customer base either by state 
+# or by job category and then charging a flat fee for each group within the segment 
+# (i.e. for each state or category). 
+# In other words they would charge different uniform prices for each state or job category. 
+# Use the data and the code to justify which approach you think would get them higher expected 
+# revenues.
+
+# Construct Prediction Function for State price discrimination
+g3 = glm(SUB~(factor(job_state))*prc,data=dz1,family='binomial')
+
+predict.rev.state = function(price,jstate="CA", cost)
+{
+  #Supress Warnings
+  options(warn=-1)
+  # "New Data"
+  nd = data.frame(job_state=jstate,prc=price)
+  # Predict
+  phat = predict(g3,nd,type='response')  
+  # Reset Warnings
+  options(warn=0)
+  # Expected Revenues
+  erev = as.numeric(phat*(price-cost))
+  return(erev)
+}
+
+# Construct Prediction Function for job category price discrimination
+g4 = glm(SUB~(factor(job_category_classified_by_ai))*prc,data=dz1,family='binomial')
+
+predict.rev.job = function(price, jcat="Legal", cost)
+{
+  #Supress Warnings
+  options(warn=-1)
+  # "New Data"
+  nd = data.frame(job_category_classified_by_ai=jcat,prc=price)
+  # Predict
+  phat = predict(g4,nd,type='response')  
+  # Reset Warnings
+  options(warn=0)
+  # Expected Revenues
+  erev = as.numeric(phat*(price-cost))
+  return(erev)
+}
+
+# Total Revenue from State discrimination
+# States
+jstate = unique(dz1$job_state)
+jstatetab = table(dz1$job_state)
+jstatetab=sort(jstatetab)
+barplot(tail(jstatetab,10))
+levels(as.factor(dz1$job_state))
+
+# Set the cost
+cost=0
+
+rev.state=0
+price.state=0
+for (i in 1:length(jstatetab)){
+  # Optimizing price for every state
+  opt = optimize(f = predict.rev.state, interval = c(0,399), jstate=names(jstatetab[i]), cost, maximum = TRUE)
+  
+  # Aggregate revenue 
+  rev.state = rev.state +jstatetab[i]*opt$objective
+  
+  # Aggregate price
+  price.state = price.state +jstatetab[i]*opt$maximum
+}
+rev.state/sum(jstatetab)
+price.state/sum(jstatetab)
+
+# Total Revenue from Job Category discrimination
+
+
+rev.job=0
+price.job=0
+for (i in 1:43){
+  # Optimizing price for every job category
+  opt = optimize(f = predict.rev.job, interval = c(0,399), jcat=jcats[i], cost, maximum = TRUE)
+  
+  # Aggregate revenue 
+  rev.job = rev.job + jtab[i]*opt$objective
+  
+  # Aggregate price
+  price.job = price.job + jtab[i]*opt$maximum
+}
+rev.job/sum(jtab)
+price.job/sum(jtab)
+
+# Total Revenue from Uniform pricing
+uni = optimize(f = predict.profit, interval = c(0,399), cost, maximum = TRUE)
+uni.rev = uni$objective; uni.rev
+uni.price = uni$maximum; uni.price
+
+# The best is job category segmentation that provides 50.35262 of rev/cust
+
+
+# Q3: Does your answer to Q2 change if you assume marginal costs per customer are $10 
+# and the decision was made based on profits rather than revenues?
+cost=10
+
+# Total Revenue from State discrimination
+
+rev.state2=0
+price.state2=0
+for (i in 1:length(jstatetab)){
+  # Optimizing price for every state
+  opt = optimize(f = predict.rev.state, interval = c(0,399), jstate=names(jstatetab[i]), cost, maximum = TRUE)
+  
+  # Aggregate revenue 
+  rev.state2 = rev.state2 +jstatetab[i]*opt$objective
+  
+  # Aggregate price
+  price.state2 = price.state2 +jstatetab[i]*opt$maximum
+}
+rev.state2/sum(jstatetab)
+price.state2/sum(jstatetab)
+
+# Total Revenue from Job Category discrimination
+rev.job2=0
+price.job2=0
+for (i in 1:43){
+  # Optimizing price for every job category
+  opt = optimize(f = predict.rev.job, interval = c(0,399), jcat=jcats[i], cost, maximum = TRUE)
+  
+  # Aggregate revenue 
+  rev.job2 = rev.job2 + jtab[i]*opt$objective
+  
+  # Aggregate price
+  price.job2 = price.job2 + jtab[i]*opt$maximum
+}
+rev.job2/sum(jtab)
+price.job2/sum(jtab)
+
+# Total Revenue from Uniform pricing
+uni2 = optimize(f = predict.profit, interval = c(0,399), cost, maximum = TRUE)
+uni.rev2 = uni2$objective; uni.rev2
+uni.price2 = uni2$maximum; uni.price2
+
+# Yes the answer change, since we will charge a higher price (inelastic demand in all cases
+# but the profits will fall for sure)
+
+#################
+# Uniform pricing
+#################
+# Prices are higher if cost is 10
+uni.price
+uni.price2
+# Revenue per customer is lower if cost is 10
+uni.rev
+uni.rev2
+
+#################
+# State pricing
+#################
+# Prices are higher if cost is 10
+price.state/sum(jstatetab)
+price.state2/sum(jstatetab)
+# Revenue per customer is lower if cost is 10
+rev.state/sum(jstatetab)
+rev.state2/sum(jstatetab)
+
+# Nonetheless, revenue per customer still higher than uniform pricing and the average price lower
+
+#################
+# Job Category pricing
+#################
+# Prices are higher if cost is 10
+price.job/sum(jtab)
+price.job2/sum(jtab)
+# Revenue per customer is lower if cost is 10
+rev.job/sum(jtab)
+rev.job2/sum(jtab)
+
+# Nonetheless, revenue per customer still higher than uniform pricing and state discrimination
+# and the average price lower than both of them
